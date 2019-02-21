f4842a364f2d6e66c57abe6dc498e7d7ab5e9c90
Xiaoji Chen <xiaoji@uber.com>
2019-02-21 10:24:14 -0800

    add performance test

--- test/render/index.js
+++ test/render/index.js
@@ -1,9 +1,12 @@
+/* global window */
 import test from 'tape-catch';
-import {SnapshotTestRunner} from '@luma.gl/test-utils';
+import {SnapshotTestRunner, PerformanceTestRunner} from '@luma.gl/test-utils';
 
 import EXAMPLE_TEST_CASES from './example-test-cases';
+import PERF_TEST_CASES from './performance-test-cases';
 
 const renderTestCaseCount = EXAMPLE_TEST_CASES.length;
+const perfTestCaseCount = PERF_TEST_CASES.length;
 
 test('RenderTest', t => {
   // tape's default timeout is 500ms
@@ -21,6 +24,25 @@ test('RenderTest', t => {
         // saveOnFail: true,
         // saveAs: '[name].png'
       }
+    })
+    .then(t.end);
+});
+
+test('PerformanceTest', t => {
+  // tape's default timeout is 500ms
+  t.timeoutAfter(perfTestCaseCount * 2000);
+
+  const pixelRatio = window.devicePixelRatio || 1;
+
+  // Mac full screen
+  new PerformanceTestRunner({width: 3600 / pixelRatio, height: 2800 / pixelRatio})
+    .add(PERF_TEST_CASES)
+    .run({
+      onTestStart: testCase => t.comment(testCase.name),
+      onTestPass: (testCase, result) =>
+        t.pass(`perf: ${result.framesRendered} frames at ${result.fps} fps`),
+      onTestFail: (testCase, result) =>
+        t.fail(`perf: ${result.framesRendered} frames at ${result.fps} fps`)
     })
     .then(t.end);
 });
---
+++ test/render/performance-test-cases.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    name: 'instancing',
+    animationLoop: require('../../examples/core/instancing/app').default,
+    targetFPS: 40
+  }
+];
